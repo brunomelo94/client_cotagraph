f46c7efb8a4611a1aa53da87c824778f0514a3d0
Bruno Melo <brunocaetano94@hotmail.com>
2023-06-17 17:59:58 -0300

    Ajustes botão de busca, e refatoração de métodos em Graph.js, correção service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,12 +4,14 @@ export default function registerServiceWorker() {
             navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
+
+                // Force the service worker to update
+                registration.update().then(() => console.log("Service Worker updated"));
+
             }, function (err) {
                 // registration failed :(
                 console.log('ServiceWorker registration failed: ', err);
             });
         });
-
-
     }
 }
